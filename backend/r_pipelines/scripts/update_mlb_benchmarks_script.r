2a65a571c96ca3ba08c0338c2d703315bc74ccea
jvillegas1021 <jvillegas1021@yahoo.com>
2026-07-08 10:49:03 -0400

    Update update_mlb_benchmarks_script.r

--- backend/r_pipelines/scripts/update_mlb_benchmarks_script.r
+++ backend/r_pipelines/scripts/update_mlb_benchmarks_script.r
@@ -2,9 +2,9 @@ library(tidyverse)
 library(DBI)
 library(RPostgres)
 
-source("data_extract_functions/extract_data_from_database.r")
-source("data_transform_functions/benchmark_functions.r")
-source("data_load_functions/load_data_to_database.r")
-source("pipelines/mlb_benchmarks_update_pipeline.r")
+source("backend/r_pipelines/data_extract_functions/extract_data_from_database.r")
+source("backend/r_pipelines/data_transform_functions/benchmark_functions.r")
+source("backend/r_pipelines/data_load_functions/load_data_to_database.r")
+source("backend/r_pipelines/pipelines/mlb_benchmarks_update_pipeline.r")
 
 run_mlb_benchmarks_update_pipeline()
