1fe95522a89bc655445c1d85a6ac558ee4f5ac8f
jvillegas1021 <jvillegas1021@yahoo.com>
2026-07-08 10:00:29 -0400

    Add files via upload

---
+++ backend/mlb-api/api_endpoints/matchup_endpoints.py
@@ -0,0 +1,94 @@
+from fastapi import APIRouter, Header, HTTPException
+import os
+from mlb_api.database import get_connection
+
+router = APIRouter()
+
+@router.get("/matchups")
+def get_matchups_today(x_api_key: str = Header(None)):
+    if x_api_key != os.getenv("API_KEY"):
+        raise HTTPException(status_code=401, detail="Invalid API Key")
+
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT * FROM matchup_df")
+    rows = cursor.fetchall()
+    colnames = [desc[0] for desc in cursor.description]
+
+    cursor.close()
+    connection.close()
+
+    return {"matchups": [dict(zip(colnames, row)) for row in rows]}
+
+
+@router.get("/pitcher_stats")
+def get_pitcher_stats(x_api_key: str = Header(None)):
+    if x_api_key != os.getenv("API_KEY"):
+        raise HTTPException(status_code=401, detail="Invalid API Key")
+
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT * FROM matchup_starting_pitcher_stats")
+    rows = cursor.fetchall()
+    colnames = [desc[0] for desc in cursor.description]
+
+    cursor.close()
+    connection.close()
+
+    return {"pitcher_stats": [dict(zip(colnames, row)) for row in rows]}
+
+
+@router.get("/pitcher_stats_current_year")
+def get_pitcher_stats_current_year(x_api_key: str = Header(None)):
+    if x_api_key != os.getenv("API_KEY"):
+        raise HTTPException(status_code=401, detail="Invalid API Key")
+
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT * FROM matchup_starting_pitcher_stats_current_year")
+    rows = cursor.fetchall()
+    colnames = [desc[0] for desc in cursor.description]
+
+    cursor.close()
+    connection.close()
+
+    return {"pitcher_stats": [dict(zip(colnames, row)) for row in rows]}
+
+
+@router.get("/team_batting_stats")
+def get_team_batting_stats(x_api_key: str = Header(None)):
+    if x_api_key != os.getenv("API_KEY"):
+        raise HTTPException(status_code=401, detail="Invalid API Key")
+
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT * FROM matchup_team_batting_stats")
+    rows = cursor.fetchall()
+    colnames = [desc[0] for desc in cursor.description]
+
+    cursor.close()
+    connection.close()
+
+    return {"team_battings_stats": [dict(zip(colnames, row)) for row in rows]}
+
+
+@router.get("/team_pitching_stats")
+def get_team_pitching_stats(x_api_key: str = Header(None)):
+    if x_api_key != os.getenv("API_KEY"):
+        raise HTTPException(status_code=401, detail="Invalid API Key")
+
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT * FROM matchup_team_pitching_stats")
+    rows = cursor.fetchall()
+    colnames = [desc[0] for desc in cursor.description]
+
+    cursor.close()
+    connection.close()
+
+    return {"team_pitching_stats": [dict(zip(colnames, row)) for row in rows]}
