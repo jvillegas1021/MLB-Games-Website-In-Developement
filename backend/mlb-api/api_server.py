9c5982619171d23e1276279cbcd7e5fbcb14601b
jvillegas1021 <jvillegas1021@yahoo.com>
2026-07-08 10:34:06 -0400

    Update api_server.py

--- backend/mlb-api/api_server.py
+++ backend/mlb-api/api_server.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from mlb_api.api_endpoints.matchup_endpoints import router as matchups_router
+from backend.mlb_api.api_endpoints.matchup_endpoints import router as matchups_router
 
 app = FastAPI()
 
