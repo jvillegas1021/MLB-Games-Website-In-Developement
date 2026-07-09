3003f4725524618b05941accb4c7c2a05287d953
jvillegas1021 <jvillegas1021@yahoo.com>
2026-07-09 15:15:11 -0400

    Update source path for MLB benchmarks pipeline

--- backend/r_pipelines/scripts/update_mlb_benchmarks_script.r
+++ backend/r_pipelines/scripts/update_mlb_benchmarks_script.r
@@ -5,6 +5,6 @@ library(RPostgres)
 source("backend/r_pipelines/data_extract_functions/extract_data_from_database.r")
 source("backend/r_pipelines/data_transform_functions/benchmark_functions.r")
 source("backend/r_pipelines/data_load_functions/load_data_to_database.r")
-source("backend/r_pipelines/pipelines/mlb_benchmarks_update_pipeline.r")
+source("backend/r_pipelines/data_pipelines/mlb_benchmarks_update_pipeline.r")
 
 run_mlb_benchmarks_update_pipeline()
