57a3e0b1d6a66b788bfbb88576e4468c1456f241
jvillegas1021 <jvillegas1021@yahoo.com>
2026-07-15 15:33:13 -0400

    Add files via upload

---
+++ backend/r_pipelines/scripts/update_mlb_games_prediction_results_script.r
@@ -0,0 +1,12 @@
+library(httr)
+library(jsonlite)
+library(tidyverse)
+library(DBI)
+library(RPostgres)
+
+source("backend/r_pipelines/data_extract_functions/extract_mlb_games_info.r")
+source("backend/r_pipelines/data_transform_functions/mlb_games_process_functions.r")
+source("backend/r_pipelines/data_load_functions/load_data_to_database.r")
+source("backend/r_pipelines/data_pipelines/mlb_games_prediction_results_pipeline_update.r")
+
+run_mlb_games_results_pipeline()
