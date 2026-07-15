6b46a371d9c66d321c6b8b25dbc4c9ecc8dd7042
jvillegas1021 <jvillegas1021@yahoo.com>
2026-07-15 15:30:50 -0400

    Add files via upload

--- backend/r_pipelines/scripts/update_mlb_benchmarks_script.r
+++ backend/r_pipelines/scripts/update_mlb_benchmarks_script.r
@@ -3,8 +3,8 @@ library(DBI)
 library(RPostgres)
 
 source("backend/r_pipelines/data_extract_functions/extract_data_from_database.r")
-source("backend/r_pipelines/data_transform_functions/benchmark_functions.r")
+source("backend/r_pipelines/data_transform_functions/mlb_games_benchmark_functions.r")
 source("backend/r_pipelines/data_load_functions/load_data_to_database.r")
-source("backend/r_pipelines/data_pipelines/mlb_benchmarks_update_pipeline.r")
+source("backend/r_pipelines/data_pipelines/mlb_games_benchmarks_update_pipeline.r")
 
-run_mlb_benchmarks_update_pipeline()
+mlb_benchmarks_update_pipeline()
