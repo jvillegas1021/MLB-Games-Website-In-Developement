8ffc7a281231c896a4c27de88718bcab9ea8dcf4
jvillegas1021 <jvillegas1021@yahoo.com>
2026-07-17 12:25:59 -0400

    Update update_mlb_games_results_script.r

--- backend/r_pipelines/scripts/update_mlb_games_results_script.r
+++ backend/r_pipelines/scripts/update_mlb_games_results_script.r
@@ -5,6 +5,7 @@ library(DBI)
 library(RPostgres)
 
 source("backend/r_pipelines/data_extract_functions/extract_mlb_games_info.r")
+source("backend/r_pipelines/data_extract_functions/extract_data_from_database.r")
 source("backend/r_pipelines/data_transform_functions/mlb_games_process_functions.r")
 source("backend/r_pipelines/data_load_functions/load_data_to_database.r")
 source("backend/r_pipelines/data_pipelines/mlb_games_results_pipeline_update.r")
